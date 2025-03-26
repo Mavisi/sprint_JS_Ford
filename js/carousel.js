b07f8a1ff60ec3b7217a1a992ccd7a5152c00907
Marcos Santana <marcos.vssjr@outlook.com>
2025-03-26 16:34:49 -0300

    responsividade home

--- js/carousel.js
+++ js/carousel.js
@@ -29,11 +29,11 @@ class Carousel {
         let titleElement = document.getElementById("carousel-title");
         
 
-        if (item      ) { //aqui eu vou att os elementos da pagina
+        if (item) { //aqui eu vou att os elementos da pagina
             imgElement.src = item.image;
-            imgElement.backgroundSize='cover'; //saber pq não funciona
+            imgElement.style.backgroundSize='cover'; //saber pq não funciona
             titleElement.innerText = item.title;
-            titleElement.innerHTML=`<a href="${item.url}"> ${item.title} </a>`; //miseravel
+            titleElement.innerHTML=`<a href="${item.url}"> ${item.title} </a>`; 
         }
 
         // Atualiza o índice para o próximo slide (loop circular)
