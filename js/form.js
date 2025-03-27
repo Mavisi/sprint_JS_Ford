dc4687900281abf5c894d61dabebb03fd2adfd0c
Marcos Santana <marcos.vssjr@outlook.com>
2025-03-27 16:53:31 -0300

    responsividade lancamento

--- js/form.js
+++ js/form.js
@@ -3,8 +3,13 @@
 
 class contato {
     
-    constructor(  ){
-
+    constructor(nome,sobrenome,email,cpf,telefone, contato ){
+        this.nome=nome;
+        this.sobrenome=sobrenome;
+        this.email=email;
+        this.cpf=cpf;
+        this.telefone=telefone;
+        this.contato=contato;
     }
 }
 
@@ -17,14 +22,27 @@ function Post(form) {
             form.elements.namedItem("telefone").value, 
             form.elements.namedItem("contato").value);
   
+    localStorage.setItem("formulario", JSON.stringify(data)); // salva no navegador/descobrir onde
+
+
+    let mostrar=JSON.parse(localStorage.getItem("formulario"));
+    console.log(mostrar);
+
+    Enviar(data.nome); // chama a função que mostra o alert
+
+
+    
+    form.reset(); // limpar o meu formulário
 }
 
-function Enviar() {
+function Enviar(nome) {
+    var nome =  document.getElementById("name");
 
-    var nome = document.getElementById("nomeid");
-
-    if (nome.value != "") {
+    if (nome) {
         alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+        console.log();
+        event.preventDefault(); // obrigado acauâ
+
     }
 
 }
